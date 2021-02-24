99b179bf876407764f94c29b639e58e0bfad3c06
Argiziont <tools33333@gmail.com>
2021-02-25 00:20:56 +0200

    Deposit fix

--- ClientApp/src/_pages/HomePage/HomePage.tsx
+++ ClientApp/src/_pages/HomePage/HomePage.tsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import VerifiedUser from "@material-ui/icons/VerifiedUser";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 import Link from "@material-ui/core/Link";
-import { UserActions } from '../../_actions';
+import { UserActions } from "../../_actions";
 import { useEffect } from "react";
-import NumberFormat from 'react-number-format';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,58 +48,36 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(5),
   },
   inputButton: {
-     width: "100%",
-     height:"200%",  
+    width: "100%",
+    height: "200%",
   },
   typography: {
     marginBottom: theme.spacing(5),
-  }
+  },
 }));
 
 export const HomePage: React.FC = () => {
   const classes = useStyles();
   const [userBalance, setUserBalance] = useState<string>();
   const [isLodaing, setIsLodaing] = useState<boolean>(true);
-  const [amountForDeposit, setAmountForDeposit] = useState<number>();
-  const [amountForWithdrawal, setAmountForWithdrawal] = useState<number>();
-
+  const [amountForDeposit, setAmountForDeposit] = useState<number>(0);
+  const [amountForWithdrawal, setAmountForWithdrawal] = useState<number>(0);
   const [userAccountState, setUserAccountState] = useState<boolean>(true);
-  
-  interface NumberFormatCustomProps {
-    inputRef: (instance: NumberFormat | null) => void;
-    onChange: (event: { target: { name: string; value: string } }) => void;
-    name: string;
-  }
-  
-  function NumberFormatCustom(props: NumberFormatCustomProps) {
-    const { inputRef, onChange, ...other } = props;
-  
-    return (
-      <NumberFormat
-        {...other}
-        getInputRef={inputRef}
-        onValueChange={(values) => {
-          onChange({
-            target: {
-              name: props.name,
-              value: values.value,
-            },
-          });
-        }}
-        thousandSeparator
-        isNumericString
-      />
-    );
-  }
 
   const handleDepositChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAmountForDeposit(Number(event.target.value));
   };
 
+  const handleWithdrawalChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setAmountForWithdrawal(Number(event.target.value));
+  };
+
   useEffect(() => {
     let isMounted = true;
     setIsLodaing(true);
-    
+
     UserActions.getBalance().then((moneyAmount) => {
       if (isMounted) {
         setUserBalance(moneyAmount);
@@ -111,140 +88,160 @@ export const HomePage: React.FC = () => {
           setIsLodaing(false);
         }
       });
-    });      
-    return () => { isMounted = false }; // use effect cleanup to set flag false, if unmounted
+    });
+    return () => {
+      isMounted = false;
+    }; // use effect cleanup to set flag false, if unmounted
   }, []);
 
-  return isLodaing?(<div></div>):(
+  return isLodaing ? (
+    <div></div>
+  ) : (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <VerifiedUser className={classes.userIcon}/>
+          <VerifiedUser className={classes.userIcon} />
         </Avatar>
 
         <Typography component="h1" variant="h5" className={classes.typography}>
           Successfully logged in
         </Typography>
-        
-         <div className={classes.root}>
+
+        <div className={classes.root}>
           <Grid container spacing={4} justify="center">
-        <Grid item xs={12}>
-            <TextField
-          variant="outlined"
-        id="outlined-size-small"
-        label="Money on account"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MonetizationOnIcon />
-              <Typography component="h1" variant="body1"   className={classes.margin}>
-                {userBalance}
-              </Typography>
-            </InputAdornment>
-          ),
-          readOnly:true
-          }}
-          />
-        </Grid>  
-        
-        <Grid item xs={12} sm={6}>
-        <TextField
-        variant="outlined"
-        id="outlined-size-small"
-                label="amountForDeposit"
-                name="amountForDeposit"
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MonetizationOnIcon />
+                      <Typography
+                        component="h1"
+                        variant="body1"
+                        className={classes.margin}
+                      >
+                        {userBalance}
+                      </Typography>
+                    </InputAdornment>
+                  ),
+                  readOnly: true,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="number"
+                variant="outlined"
                 value={amountForDeposit}
-        onChange={handleDepositChange}
+                onChange={handleDepositChange}
                 InputProps={{
                   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                  inputComponent: NumberFormatCustom as any,
-          startAdornment: (
-            <InputAdornment position="start">
-              <MonetizationOnIcon />
-              
-            </InputAdornment>
-          )
-          }}
-          />
-          
-        </Grid>
-              <Grid item xs={12} sm={6}>
-              <Button size="large" variant="contained" onClick={async () => {
-                await UserActions.depositToAccount(amountForDeposit || 0);
-
-                UserActions.getBalance().then((moneyAmount) => {
-                  setUserBalance(moneyAmount);
-                });
-                setAmountForDeposit(0);
-              
-              }}>
-              <Typography component="h1" variant="body1"   className={classes.margin}>
-                Submit
-              </Typography>
-        </Button>
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MonetizationOnIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
-              <Grid item xs={12} sm={6}>
-        <TextField
-          variant="outlined"
-        id="outlined-size-small"
-        label="amountForwithdrawal"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MonetizationOnIcon />
-             
-            </InputAdornment>
-          )
-          }}
-          />
-        </Grid>
-        
-              <Grid item xs={12} sm={6}>
-              <Button size="large" variant="contained" >
-              <Typography component="h1" variant="body1"   className={classes.margin}>
-                Submit
-              </Typography>
-        </Button>
-              </Grid>
-        <Grid item>
-            <FormControlLabel
-        control={
+            <Grid item xs={12} sm={6}>
+              <Button
+                size="large"
+                variant="contained"
+                onClick={async () => {
+                  await UserActions.depositToAccount(amountForDeposit || 0);
+
+                  UserActions.getBalance().then((moneyAmount) => {
+                    setUserBalance(moneyAmount);
+                  });
+                  setAmountForDeposit(0);
+                }}
+              >
+                <Typography
+                  component="h1"
+                  variant="body1"
+                  className={classes.margin}
+                >
+                  Submit
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                value={amountForWithdrawal}
+                onChange={handleWithdrawalChange}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MonetizationOnIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button size="large" variant="contained">
+                <Typography
+                  component="h1"
+                  variant="body1"
+                  onClick={async () => {
+                    await UserActions.withdrawalFromAccout(
+                      amountForWithdrawal || 0
+                    );
+
+                    UserActions.getBalance().then((moneyAmount) => {
+                      setUserBalance(moneyAmount);
+                    });
+                    setAmountForWithdrawal(0);
+                  }}
+                  className={classes.margin}
+                >
+                  Submit
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
                   <Switch
                     size="medium"
                     name="AccountOpenState"
                     color="primary"
                     checked={userAccountState}
                     onChange={async () => {
-                      
                       if (userAccountState) {
                         await UserActions.closeAccount();
                       }
-                      if (!userAccountState){
+                      if (!userAccountState) {
                         await UserActions.openAccount();
                       }
-                      
+
                       setUserAccountState(!userAccountState);
                     }}
-          />
-        }
-        label="Account state"
-      />
+                  />
+                }
+                label="Account state"
+              />
             </Grid>
-        <Grid item xs={12} sm={12}>
-              <Link href="#" variant="body2" onClick={
-                () => {
+            <Grid item xs={12} sm={12}>
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => {
                   UserActions.logout();
                 }}
               >
                 {"Want to exit? Log out"}
               </Link>
             </Grid>
-      </Grid>
+          </Grid>
         </div>
-        
       </div>
     </Container>
   );
 };
-
